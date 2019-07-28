43fd467a3cb1f295a2a5914a3dd1da4679905ba3
George Ruiz <georuiz817@gmail.com>
2019-07-28 20:28:23 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,11 @@
- class CreateCostumes < ActiveRecord::Migration
-  def change 
-    create_table :costumes do |t|
-      t.string :name
-      t.float :price
-      t.string :image_url
-      t.string :size
- 
-    end
-  end
-end
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.integer :price
+            t.string :size
+            t.string :image_url
+            t.timestamps
+        end 
+    end 
+end 
